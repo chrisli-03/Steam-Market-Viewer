450310d5f51c3d881c9316265ac457e82097fc45
kunxiaozheng <kunxiaozheng@gmail.com>
2016-08-20 22:23:57 -0400

    added scanner for user input

--- src/Testing.java
+++ src/Testing.java
@@ -2,6 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
+import java.util.Scanner; 
 
 public class Testing {
 
@@ -17,7 +18,10 @@ public class Testing {
 			return Integer.toString(value);
 		}
 	};
-	public static String convertIntoURLName(String input){
+	public static String convertIntoURLName(){
+		System.out.println("Please enter the item name:");		
+		Scanner scan = new Scanner(System.in);
+		String input = scan.nextLine();
 		String [] splitedString=input.split(" ");
 		String result= splitedString[0];
 		for(int i =1;i<splitedString.length;i++){
@@ -30,7 +34,7 @@ public class Testing {
     public static void main(final String[] args) throws IOException {
         BufferedReader reader = null;
         try {
-        	String setName=convertIntoURLName("Platinum Baby Roshan");
+        	String setName=convertIntoURLName();
         	String game = Games.DOTA2.getValue();
             String name="http://steamcommunity.com/market/priceoverview/?appid="+game+"&market_hash_name="+ setName;
             URL url = new URL(name);
