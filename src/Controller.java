66b5967d74b5d3d5487493ece2d49aa8e8d67940
kunxiaozheng <kunxiaozheng@gmail.com>
2016-08-21 01:27:32 -0400

    setup the layout and added text field

--- src/Controller.java
+++ src/Controller.java
@@ -1,5 +1,8 @@
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+
+import javax.swing.JButton;
 
 class Controller implements ActionListener {
 	private Model model;
@@ -9,7 +12,11 @@ class Controller implements ActionListener {
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		
+			if (e.getSource() instanceof JButton) {
+				Object source = e.getSource();
+				JButton button = (JButton) source;
+				System.out.println(button.getText());
+			}
 	}
 
 }
